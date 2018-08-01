ee720bad598fab525cb32380f9a1209b56af84c1
dior4ik <dior4ik@gmail.com>
2018-08-01 16:40:05 +0300

    bug fixed
    serach algorithm fixed

--- 4sq_wifi_pass_finder.py
+++ 4sq_wifi_pass_finder.py
@@ -4,39 +4,75 @@
 import json
 import httplib
 
+nbr_req_agree = "n"
+req_counter = 1
+
 C_ID = str(raw_input("Enter CLIENT ID of your 4sq app: "))
 C_SCRT = str(raw_input("Enter CLENT SECRET of your 4sq app: "))
 
 yx1 = raw_input("Enter lower left coordinate pair from Google Maps in format 'latitude,longitude': ").split(",")
 yx2 = raw_input("Enter upper right coordinate pair from Google Maps in format 'latitude,longitude': ").split(",")
 
-y1 = float(yx1[0])
-x1 = float(yx1[1])
-y2 = float(yx2[0])
-x2 = float(yx2[1])
-
 print
-print "\033[33mATTENTION!!! Next parameter 'step' must be chosen based on the fact that the maximum number of searching results at the specified place is 500." \
-      " If you choose too large 'step', all of nearby places for specified coordinate might not be included to the search results.\033[0m"
+print "\033[33mATTENTION!!! Next parameter 'step' must be chosen based on the fact that you can do only 500 requests per hour." \
+      " If you choose too small 'step', you will be blocked by API server for 1 hour.\033[0m"
 print
 raw_input("\033[32mPlease, press Enter to continue\033[0m")
 print
 
-raw_step = raw_input("Enter step for increasing lower left coordinates to upper right coordinates (recommended step is 500 meters): ")
-step = float(raw_step) / 1000000
+while nbr_req_agree != "Y":
+    nbr_req = 1
+    y1 = float(yx1[0])
+    y2 = float(yx2[0])
+
+    print
+    raw_step = raw_input("Enter step for increasing lower left coordinates to upper right coordinates (recommended step is 1000 meters): ")
+    print
+    step = float(raw_step) / 100000
+
+    while y1 < y2:
+        x1 = float(yx1[1])
+        x2 = float(yx2[1])
+        while x1 < x2:
+            nbr_req += 1
+            x1 = x1 + step
+        else:
+            x1 = x2
+            nbr_req += 1
+        y1 = y1 + step
+    else:
+        y1 = y2
+        x1 = float(yx1[1])
+        x2 = float(yx2[1])
+        while x1 < x2:
+            nbr_req += 1
+            x1 = x1 + step
+        else:
+            x1 = x2
+
+    print
+    print "Number of requests with step " + str(raw_step) + " meters: " + str(nbr_req)
+    print
+
+    nbr_req_agree = raw_input("\033[33mATTENTION!!! Enter 'Y' if you want to continue or 'n' if you want to set another step value: \033[0m")
+
+else:
+    y1 = float(yx1[0])
+    x1 = float(yx1[1])
+    y2 = float(yx2[0])
+    x2 = float(yx2[1])
+
+    print "Lower left coordinate pair from Google Maps is: " + str(y1) + ", " + str(x1)
+    print "Upper right coordinate pair from Google Maps is: " + str(y2) + ", " + str(x2)
+    print "Step is: " + str(raw_step) + " meters"
+
 
 out_file = raw_input("Enter name for output file: ")
 out_file = open(out_file, "a+")
 
-print "Lower left coordinate pair from Google Maps is: " + str(y1) + ", " + str(x1)
-print "Upper right coordinate pair from Google Maps is: " + str(y2) + ", " + str(x2)
-print "Step is: " + str(step)
-
 print
 raw_input("\033[32mPlease, press Enter to start searching\033[0m")
 print
-
-req_counter = 1
 
 def data_request():
     connection = httplib.HTTPSConnection('api.foursquare.com')
@@ -53,18 +89,32 @@ def response_parser(x):
         results = i['venue']['id'] + ',' + i['venue']['name'] + ',' + str(i['venue']['location']['lat']) + ',' + str(i['venue']['location']['lng']) + ',' + i['id'] + ',' + i['text'] + "\n"
         out_file.write(results.encode('utf-8'))
 
+
+y1 = float(yx1[0])
+y2 = float(yx2[0])
+
 while y1 < y2:
+    x1 = float(yx1[1])
+    x2 = float(yx2[1])
     while x1 < x2:
         response_parser(data_request())
-        x1 = x1 + step
+        x1 += step
         req_counter += 1
     else:
         x1 = x2
         response_parser(data_request())
         req_counter += 1
-    y1 = y1 + step
-
+    y1 += step
 else:
     y1 = y2
-    response_parser(data_request())
-    req_counter += 1
+    x1 = float(yx1[1])
+    x2 = float(yx2[1])
+    while x1 < x2:
+        response_parser(data_request())
+        x1 += step
+        req_counter += 1
+    else:
+        x1 = x2
+        response_parser(data_request())
+
+print "Congrats!!! Your search is completed"
